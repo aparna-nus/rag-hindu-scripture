f91d8f3c6e86656cf4d4620b08dbd60857a04a88
aparnakrishna <aparnakrishna.work@gmail.com>
2025-11-11 21:57:18 +0800

    refactoring for github pages error

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
 
   // FOR LOCAL DEVELOPMENT CHANGE TO: base: '/'
-  base: '/rag-hindu-scripture/web/', // for GitHub Pages deployment
+  base: '/rag-hindu-scripture/', // for GitHub Pages deployment
 
 
   //server: { // local dev only
